6d92780a62007b7e45711b181e0d9d5b9c9e3a64
ShahinRafei <rafeishahin@gmail.com>
2024-02-23 21:47:52 +0330

    add men's and woman's clothing collection banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,10 @@
     <HeaderSwiperComponent />
     <CategoriesComponent />
     <OffProductsComponent />
+    <div class="container mx-auto flex gap-2 items-center overflow-hidden">
+      <img src="@/assets/images/woman_clothing_off.webp" alt="woman's clothing collection off" class="w-1/2 object-contain rounded-md shadow-md shadow-slate-400 border-2">
+      <img src="@/assets/images/man_clothing_off.jpg" alt="man's clothing collection off" class="w-1/2 object-contain rounded-md shadow-md shadow-slate-400 border-2">
+    </div>
   </div>
 </template>
 
