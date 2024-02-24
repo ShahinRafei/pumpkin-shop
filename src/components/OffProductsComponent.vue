acd717f891023ceb41b0d97b70a37e4394a86561
ShahinRafei <rafeishahin@gmail.com>
2024-02-24 12:57:42 +0330

    fetch product api to off component

--- src/components/OffProductsComponent.vue
+++ src/components/OffProductsComponent.vue
@@ -7,16 +7,16 @@
                     <img src="@/assets/images/up-to-off-percentage-15.png" alt="" class="w-full h-full object-cover">
                 </div>
             </swiper-slide>
-            <swiper-slide v-for="item in 10" :key="item">
+            <swiper-slide v-for="product in productsList" :key="product">
                 <div class="w-52 bg-white p-3 shadow-md rounded-md">
                     <div class="w-full h-52 mb-2 rounded-t-md overflow-hidden border-b-2">
-                        <img src="@/assets/images/banner1.webp" alt="" class="w-full h-full object-contain">
+                        <img :src="product.image" :alt="product.title" class="w-full h-full object-contain">
                     </div>
-                    <div class="w-full mb-2 text-lg font-serif whitespace-nowrap text-ellipsis overflow-hidden">title</div>
+                    <div class="w-full mb-2 font-serif whitespace-nowrap text-ellipsis overflow-hidden">{{ product.title }}</div>
                     <div class="flex justify-between items-center mb-5">
                         <div class="flex items-end gap-1">
-                            <span class="font-semibold">$120.99</span>
-                            <span class="text-slate-400 line-through text-xs">$190.99</span>
+                            <span class="font-semibold">${{ product.price }}</span>
+                            <span class="text-slate-400 line-through text-xs">${{ parseFloat(product.price * 1.1).toFixed(2) }}</span>
                         </div>
                         <div class="bg-orange-600 rounded-full px-3 text-white font-mono">10%</div>
                     </div>
@@ -27,19 +27,35 @@
 </template>
 
 <script setup>
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
+//imports
+import store from '@/store';
+import { computed, ref, onMounted } from 'vue';
 
-// Import Swiper styles
+//import swiper in component
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { FreeMode, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 
-// import required modules
-import { FreeMode, Navigation } from 'swiper/modules';
-
 let modules = [FreeMode, Navigation];
 
+//data
+const showSkeleton = ref(false);
+
+//api
+function getProductsList(){
+    return store.dispatch('getProductsListFromApi');
+}
+const productsList = computed(()=>{
+    return store.getters.getProductsList;
+});
+
+onMounted(async() => {
+    showSkeleton.value = true;
+    await getProductsList();
+    showSkeleton.value = false;
+})
 </script>
 
 <style scoped>
