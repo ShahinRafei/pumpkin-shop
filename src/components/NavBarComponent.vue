88c62380b7f31162390be9d865e2e0971863461b
ShahinRafei <rafeishahin@gmail.com>
2024-03-30 15:40:13 +0330

    fix navbar categories address

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -5,7 +5,7 @@
             <ul
                 class="w-1/2 sm:w-1/3 lg:w-full flex flex-col lg:flex-row gap-1 lg:gap-2 bg-white shadow-md lg:shadow-none rounded-b-md ml-2 sm:m-0 px-2 py-3 sm:px-3 lg:py-1 text-neutral-600">
                 <li v-for="categori in categories" :key="categori">
-                    <router-link to="/"
+                    <router-link :to="`/categories/${categori}`"
                         active-class="!bg-orange-100 hover:bg-neutral-200 lg:!bg-transparent lg:before:absolute lg:before:w-full lg:befor lg:before:h-1 lg:before:-bottom-1 lg:before:left-0 lg:before:bg-orange-400 lg:before:rounded-t-md lg:hover:!text-neutral-600"
                         class="relative hover:bg-neutral-200 lg:hover:bg-transparent lg:hover:text-orange-600 py-2 lg:py-1 px-4 rounded-md block">
                         {{ categori }}
