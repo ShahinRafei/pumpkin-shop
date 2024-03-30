0a5b1c1917fda27c6cb8d357ac35041a579a5d94
ShahinRafei <rafeishahin@gmail.com>
2024-03-31 00:24:33 +0330

    add link to first page products

--- src/components/OffProductsComponent.vue
+++ src/components/OffProductsComponent.vue
@@ -1,26 +1,34 @@
 <template>
     <div
         class="container mx-auto bg-gradient-to-b from-orange-700/5 to-orange-600/60 rounded-lg shadow-inner outline outline-4 outline-orange-700/5 py-2 lg:py-5 px-1 my-2 sm:my-5">
-        <swiper :navigation="true" :slidesPerView="'auto'" :grabCursor="true" :spaceBetween="5" :freeMode="true" :modules="modules">
+        <swiper :navigation="true" :slidesPerView="'auto'" :grabCursor="true" :spaceBetween="5" :freeMode="true"
+            :modules="modules">
             <swiper-slide>
                 <div class="w-28 sm:w-36 md:w-44 lg:w-52 pl-5 opacity-80">
                     <img src="@/assets/images/up-to-off-percentage-15.png" alt="" class="w-full h-full object-cover">
                 </div>
             </swiper-slide>
             <swiper-slide v-for="product in productsList" :key="product">
-                <div class="w-28 sm:w-36 md:w-44 lg:w-52 bg-white p-3 shadow-md rounded-md">
-                    <div class="w-full h-20 sm:h-32 md:h-40 lg:h-48 mb-2 rounded-t-md overflow-hidden border-b-2">
-                        <img :src="product.image" :alt="product.title" class="w-full h-full object-contain">
-                    </div>
-                    <div class="w-full mb-2 font-serif whitespace-nowrap text-ellipsis overflow-hidden text-xs md:text-sm lg:text-base">{{ product.title }}</div>
-                    <div class="flex justify-between items-start flex-wrap sm:mb-2">
-                        <div class="flex flex-col items-start gap-1 text-xs md:text-sm lg:text-base">
-                            <span class="font-semibold">${{ product.price }}</span>
-                            <span class="text-slate-400 line-through text-xs">${{ parseFloat(product.price * 1.1).toFixed(2) }}</span>
+                <router-link :to="{ name: 'product', params: { id: product.id } }">
+                    <div class="w-28 sm:w-36 md:w-44 lg:w-52 bg-white p-3 shadow-md rounded-md">
+                        <div class="w-full h-20 sm:h-32 md:h-40 lg:h-48 mb-2 rounded-t-md overflow-hidden border-b-2">
+                            <img :src="product.image" :alt="product.title" class="w-full h-full object-contain">
                         </div>
-                        <div class="bg-orange-600 rounded-full px-2 md:px-3 text-white font-mono text-xs md:text-sm lg:text-base">10%</div>
+                        <div
+                            class="w-full mb-2 font-serif whitespace-nowrap text-ellipsis overflow-hidden text-xs md:text-sm lg:text-base">
+                            {{ product.title }}</div>
+                        <div class="flex justify-between items-start flex-wrap sm:mb-2">
+                            <div class="flex flex-col items-start gap-1 text-xs md:text-sm lg:text-base">
+                                <span class="font-semibold">${{ product.price }}</span>
+                                <span class="text-slate-400 line-through text-xs">${{ parseFloat(product.price *
+                                    1.1).toFixed(2) }}</span>
+                            </div>
+                            <div
+                                class="bg-orange-600 rounded-full px-2 md:px-3 text-white font-mono text-xs md:text-sm lg:text-base">
+                                10%</div>
+                        </div>
                     </div>
-                </div>
+                </router-link>
             </swiper-slide>
         </swiper>
     </div>
@@ -44,22 +52,20 @@ let modules = [FreeMode, Navigation];
 const showSkeleton = ref(false);
 
 //api
-function getProductsList(){
+function getProductsList() {
     return store.dispatch('getProductsListFromApi');
 }
-const productsList = computed(()=>{
+const productsList = computed(() => {
     return store.getters.getProductsList;
 });
 
-onMounted(async() => {
+onMounted(async () => {
     showSkeleton.value = true;
     await getProductsList();
     showSkeleton.value = false;
 })
 </script>
 
-<style scoped>
-.swiper-slide {
+<style scoped>.swiper-slide {
     width: auto;
-}
-</style>
+}</style>
--- src/components/PopularProductsComponent.vue
+++ src/components/PopularProductsComponent.vue
@@ -1,26 +1,35 @@
 <template>
-    <div class="container border-2 border-amber-700/10 rounded-xl mx-auto mt-5 p-3 flex flex-col items-center bg-amber-700/5 shadow-inner">
+    <div
+        class="container border-2 border-amber-700/10 rounded-xl mx-auto mt-5 p-3 flex flex-col items-center bg-amber-700/5 shadow-inner">
         <h2 class="font-semibold text-slate-600 text-2xl pb-10 pt-2">Popular Products</h2>
         <div class="w-full grid grid-cols-table-sm sm:grid-cols-table justify-items-center gap-5">
-            <div v-for="product in productsList" :key="product"
-                class="w-32 sm:w-40 md:w-48 lg:w-52 bg-white p-3 rounded-md shadow-md flex flex-col justify-center items-center border-2 border-red-100 hover:scale-105 hover:border-red-300 transition-transform">
-                <img :src="product.image" :alt="product.title" class="w-full aspect-square object-contain border-b-2 pb-5">
-                <div class="w-full h-full flex flex-col justify-between items-center">
-                    <div class="w-full pt-2 text-center text-xs md:text-sm lg:text-base">
-                        {{ product.title }}
-                    </div>
-                    <div class="relative flex flex-col max-w-max">
-                        <div class="absolute w-full flex gap-1">
-                            <span v-for="number in 5" :key="number" class="text-sm md:text-base lg:text-lg text-red-700 opacity-50">♥</span>
+            <template v-for="product in productsList" :key="product">
+                <router-link :to="{ name: 'product', params: { id: product.id } }">
+                    <div
+                        class="h-full w-32 sm:w-40 md:w-48 lg:w-52 bg-white p-3 rounded-md shadow-md flex flex-col justify-center items-center border-2 border-red-100 hover:scale-105 hover:border-red-300 transition-transform">
+                        <img :src="product.image" :alt="product.title"
+                            class="w-full aspect-square object-contain border-b-2 pb-5">
+                        <div class="w-full h-full flex flex-col justify-between items-center">
+                            <div class="w-full pt-2 text-center text-xs md:text-sm lg:text-base">
+                                {{ product.title }}
+                            </div>
+                            <div class="relative flex flex-col max-w-max">
+                                <div class="absolute w-full flex gap-1">
+                                    <span v-for="number in 5" :key="number"
+                                        class="text-sm md:text-base lg:text-lg text-red-700 opacity-50">♥</span>
+                                </div>
+                                <div class="relative flex gap-1 bg-red-700 bg-clip-text overflow-hidden"
+                                    :style="`width: ${product.rating.rate * 20}%`">
+                                    <span v-for="number in 5" :key="number"
+                                        class="text-sm md:text-base lg:text-lg text-transparent">♥</span>
+                                </div>
+                                <div class="text-center text-slate-400 text-xs md:text-sm lg:text-base">({{
+                                    parseInt(product.rating.count) }} votes)</div>
+                            </div>
                         </div>
-                        <div class="relative flex gap-1 bg-red-700 bg-clip-text overflow-hidden"
-                            :style="`width: ${product.rating.rate * 20}%`">
-                            <span v-for="number in 5" :key="number" class="text-sm md:text-base lg:text-lg text-transparent">♥</span>
-                        </div>
-                        <div class="text-center text-slate-400 text-xs md:text-sm lg:text-base">({{ parseInt(product.rating.count) }} votes)</div>
                     </div>
-                </div>
-            </div>
+                </router-link>
+            </template>
         </div>
     </div>
 </template>
