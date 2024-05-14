0b35b7538c9e4a7803416a21e655f1e5a11397c4
ShahinRafei <rafeishahin@gmail.com>
2024-05-15 00:32:26 +0330

    add loading component

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="fixed z-[999] w-screen h-screen flex justify-center items-center bg-black/20 backdrop-blur-sm">
+        <div class="flex items-end animate-pulse">
+            <img src="@/assets/images/logo.png" alt="pumpkin-shop" class="h-12">
+            <span
+                class="font-bold text-3xl bg-gradient-to-r from-orange-500 from-40% to-green-600 bg-clip-text text-transparent">Pumpkin</span>
+        </div>
+    </div>
+</template>
